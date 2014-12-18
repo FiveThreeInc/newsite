3b6bd25b8b0b2c5bf5b350f8765a5d9740f96422
peg <peg@pauls-mbp>
2014-12-18 03:32:53 -0500

    Redirect Pages Basic 403, 404

--- 404/index.php
+++ 404/index.php
@@ -1,3 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Five 3 - 404</title>
 
-echo "404 | Page Not Found";
+        <!--General Schemas-->
+        <meta content="Team Five Three: Designing and developing rich web and mobile applications. " name="description">
+        <meta name="viewport" content=" width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+        <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+
+        <!--Google Schemas-->
+
+        <!--Google Resources-->
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+        <!--Host Resources-->
+        <link rel="stylesheet" type="text/css" href="http://beta.fivethreeinc.com/css/default.css/"/>
+
+        <!--TESTING-->
+        <script type="text/javascript">
+
+        </script>
+        <style type="text/css">
+
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1><a href="">404</a></h1>
+        </header>
+        <?php echo "404 | Page Not Found"; ?>
+
+    </body>
+</html>
