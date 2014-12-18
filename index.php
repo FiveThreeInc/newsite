1d2b2f7d3ef094eafd01544d5f3422613238d61e
peg <peg@pauls-mbp>
2014-12-17 23:25:28 -0500

    Index Update Default CSS Update

--- index.php
+++ index.php
@@ -1,4 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Five 3</title>
+        
+        <!--General Schemas-->
+        <meta content="Team Five Three: Designing and developing rich web and mobile applications. " name="description">
+        <meta name="viewport" content=" width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+        <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+        
+        <!--Google Schemas-->
+        
+        <!--Google Resources-->
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        
+        <!--Host Resources-->
+        <link rel="stylesheet" type="text/css" href="http://beta.fivethreeinc.com/css/default.css/"/>
+        
+        <!--TESTING-->
+        <script type="text/javascript">
 
-echo "basic goals : 1) Inform Visitors 2) Navigate Users";
-    
+        </script>
+        <style type="text/css">
+
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>Team Five Three</h1>
+            <nav>
+                <ul>
+                    <li><a href="">News</a></li>
+                    <li><a href="">Team</a></li>
+                    <li><a href="">Work</a></li>
+                    <li><a href="">Products</a></li>
+                    <li><a href="">Games</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
