2e3b24359871b9e93a36b0605d213663ca8d6519
peg <peg@pauls-mbp>
2014-12-19 18:29:21 -0500

    Redirect Pages Updated 	Made CSS Changes to 404 & 403
    Icons.PHP added to Site Objects Folder
    	Researched and optimized favicons and web clips for most browsers

--- 404/index.php
+++ 404/index.php
@@ -2,33 +2,46 @@
 <html>
     <head>
         <title>Five 3 - 404</title>
-
         <!--General Schemas-->
+        <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
         <meta content="Team Five Three: Designing and developing rich web and mobile applications. " name="description">
         <meta name="viewport" content=" width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
-        <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+
+        <!--WE SHOULD MAKE A LARGER OBJECT CALLED BRAINS-->
+
+        <!--IMPORT CX SITE ICONS-->
+        <?php require_once('../siteobjects/icons.php'); ?>
 
         <!--Google Schemas-->
 
-        <!--Google Resources-->
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <!--Google Resources Removed See Style Comment-->
 
-        <!--Host Resources-->
-        <link rel="stylesheet" type="text/css" href="http://beta.fivethreeinc.com/css/default.css/"/>
+        <!--Host Resources Removed See Style Comment-->
 
         <!--TESTING-->
         <script type="text/javascript">
 
         </script>
         <style type="text/css">
+            /* This page doesn't share styles with the other pages, and is designed to load quickly, so we'll just use a couple of inline styles here. */
+            html {background: rgb(22,22,22); box-sizing: border-box;  overflow: hidden;  height: 100%;  width: 100%;  position: relative;  padding: 0px;  }
+            body {height: 100%;  width: 100%; margin: 0px;  padding: 0px 10%;  background: transparent;  box-sizing: border-box;  overflow: hidden;  position: relative;  font-family: Helvetica Neue, Helvetica, sans-serif;  font-size: 13px;  color: rgb(255,255,255);  line-height: 16px;  text-align: center;  -webkit-font-smoothing: antialiased;  }
+            /*HEADER*/
+            /*MAIN MINI*/
+            header {background: transparent;  box-sizing: border-box;  height: auto;  width: 100%;  padding-top: 20%;  }
+            header h1 {font-size: 0px;  background-repeat: no-repeat;  background-position: center;  background-color: transparent;  background-size: 60px;  background-image: url("/img/53small.png");  width: 60px;  height: 60px;  margin-top: 0px;  margin-left: auto;  margin-right: auto;  margin-bottom: 23px;  }
+            header h1 a {box-sizing: border-box;  overflow: hidden;  color: orange;  text-align: left;  text-decoration: none;  display: block;  height: 100%;  width: 100%;  padding: 12px 0px 12px 10px;  }
 
+            /*ERROR PAGE TITLE & DESCRIPTION*/
+            span{display: block;padding-bottom: 18px;margin: 0px auto;}
+            span.title{font-size: 18px;-webkit-text-size-adjust: 100%; /* Prevent font scaling in landscape while allowing user zoom */}
+            span.description{color: rgb(232,232,232);max-width: 300px;font-size: 13px;-webkit-text-size-adjust: 100%; /* Prevent font scaling in landscape while allowing user zoom */}
+            span.description a{color:rgb(167,167,167);text-decoration: none;}
         </style>
     </head>
     <body>
-        <header>
-            <h1><a href="">404</a></h1>
-        </header>
-        <?php echo "404 | Page Not Found"; ?>
-
+        <header><h1><a href="/beta">404</a></h1></header>
+        <span class="title">404 - Page Not Found</span>
+        <span class="description">If Five 3 does not redirect, click our icon above or visit <a href="/">www.fivethreeinc.com</a>.</span>
     </body>
 </html>
---
+++ siteobjects/icons.php
@@ -0,0 +1,24 @@
+<!--Apple Touch Icon Links-->
+<link rel="apple-touch-icon" href="/img/icons/touch-icon-iphone.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/img/icons/touch-icon-ipad.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/img/icons/touch-icon-iphone-retina.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/img/icons/touch-icon-ipad-retina.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/img/icons/touch-icon-iphone-retina-plus.png">
+
+<!--APPLE iOS 6 IMAGE-->
+<link rel="apple-touch-icon-precomposed" href="/img/icons/touch-icon-iphone-retina.png">
+
+<!--APPLE STARTUP IMAGE-->
+<link rel="apple-touch-startup-image" href="/img/icons/startup.png">
+
+<!--WINDOWS 8 TILE Icons-->
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/img/icons/favicon96.png">
+
+<!--Standard Icon-->
+<link rel="icon" type="image/ico" href="/img/icons/favicon.ico"/>
+
+<!--Other Icon Links-->
+<link rel="icon" sizes="16x16" type="image/x-icon" href="/img/icons/favicon.ico">
+<link rel="icon" sizes="96x96" type="image/png" href="/img/icons/favicon96.png" />
+
